78647fe40717883a816ac8e88cd24b0a1527444a
Amanop69 <sinescanordeadly@gmail.com>
2022-07-08 16:10:35 +0000

    Amanop69

---
+++ folio/src/container/About/About.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
+import { Typewriter } from 'react-simple-typewriter'
+
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">"IF <span style={{ color: 'blue', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['YOU CAN MAKE IT LOOK NICE THEN YOU CAN MAKE IT ACTUALLY NICE..."']}
+            loop={2}
+            cursor
+            cursorStyle='</>'
+            typeSpeed={100}
+            deleteSpeed={10}
+            delaySpeed={1000}
+          />
+        </span></h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
